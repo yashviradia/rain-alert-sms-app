3896be6b8e0f4759321e1684f2e2b34303147df6
yashviradia <63194797+yashviradia@users.noreply.github.com>
2021-12-19 11:04:28 +0100

    change in comment

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ owm_api_key = os.environ.get("OWM_API_KEY")
 account_sid = os.environ.get("TWILIO_ACCOUNT_SID")
 auth_token = os.environ.get("TWILIO_AUTH_TOKEN")
 
-# lat and lon must be private
 parameters = {
     "lat": 41.015137,
     "lon": 28.979530,
@@ -34,7 +33,6 @@ for hour_data in weather_slice:
     if int(condition_code) < 700:
         will_rain = True
 
-# Natelnummer muss privat dargestellt werden
 if will_rain:
     client = Client(account_sid, auth_token)
 
